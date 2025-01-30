4d74a1b3b7ecc67699e041c71299b24e9e59edfe
Miguel Rivera <miguelrivera@Miguels-MacBook-Air.local>
2025-01-30 19:03:17 +0100

    fix: :hammer: update npm for OC

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,23 +1,11 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+  const deploymentUrl = process.env.DEPLOYMENT_URL || 'http://localhost:3000';
 
   it('/hello (GET)', () => {
-    return request(app.getHttpServer())
+    return request(deploymentUrl)
       .get('/hello')
       .expect(200)
       .expect('Hello World!');
